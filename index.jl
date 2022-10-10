6d215abed9a07e984156ea56efbb3dbc3be33245
Aschreyju <79573511+Aschreyju@users.noreply.github.com>
2022-10-10 05:37:49 +0000

    Deploying to gh-pages from  @ 068e531f27992ad25d6710e9d59f815a500a08c2 🚀

--- index.jl
+++ index.jl
@@ -11,6 +11,7 @@ This is the index of the website.
 Here are the notebooks.
 
 * [Dunning Kruger](https://aschreyju.github.io/website2/DunningKrugerRandom.html)
+* [Cool Notebook](https://aschreyju.github.io/website2/DunningKrugerRandom.html/My_cool_notebook.html)
 
 
 """
