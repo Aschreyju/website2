d85de139717c48da8f750f98e3b070a48681fc8f
Aschreyju <79573511+Aschreyju@users.noreply.github.com>
2022-10-10 12:05:25 +0800

    Add files via upload

---
+++ index.jl
@@ -0,0 +1,37 @@
+### A Pluto.jl notebook ###
+# v0.19.12
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 605c66ce-484f-11ed-110c-154a833bf69f
+md"""
+This is the index of the website.
+
+Here are the notebooks.
+
+* [Dunning Kruger](https://aschreyju.github.io/website2/DunningKrugerRandom.html)
+
+
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.2"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─605c66ce-484f-11ed-110c-154a833bf69f
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
